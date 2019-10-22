049fe6f1946d31f450fa70144916610227716347
Jessica Scazzero <Jessica@dhcp-10-250-131-234.harvard.edu>
2019-10-22 12:27:16 -0400

    Shinny App

--- showing_off/app.R
+++ showing_off/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Jessica's Geyser Data"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
